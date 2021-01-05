430c6c3a0ab1284b423c410b9fe19c49ba77d0f0
Lin Song <song.lin@volvo.com>
2021-01-05 16:37:43 +0800

    updated

---
+++ ServiceDiscovery/SelfRegistration/src/SalesAPI/Startup.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Consul;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using SalesAPI.Infrastructure;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace SalesAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<DataStore>();
+            services.AddSingleton<IHostedService, ConsulHostedService>();
+            services.Configure<ConsulConfig>(Configuration.GetSection("consulConfig"));
+            services.AddSingleton<IConsulClient, ConsulClient>(p => new ConsulClient(consulConfig =>
+            {
+                var address = Configuration["consulConfig:address"];
+                consulConfig.Address = new Uri(address);
+            }));
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("default_policy", pb =>
+                {
+                    pb.AllowAnyHeader()
+                      .AllowAnyMethod()
+                      .AllowAnyOrigin();
+                });
+            });
+
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Sales API (Local Registry)",
+                    Description = "Sales API "
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApplicationLifetime lifetime)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            app.UseSwagger(c =>
+            {
+                c.PreSerializeFilters.Add((swagger, httpReq) => swagger.Host = httpReq.Host.Value);
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1 Docs");
+            });
+
+            app.UseWelcomePage();
+        }
+    }
+}
---
+++ ServiceDiscovery/SelfRegistration/src/SalesClient/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace SalesClient
+{
+    public class Program
+    {
+        private static IConfigurationRoot _configuration;
+        private static ApiClient _apiClient;
+        public static void Main(string[] args)
+        {
+            LoadConfig();
+            var logger = new LoggerFactory().AddConsole().CreateLogger<ApiClient>();
+            _apiClient = new ApiClient(_configuration, logger);
+
+            try
+            {
+                _apiClient.Initialize().Wait();
+
+                ListStudents().Wait();
+                ListCourses().Wait();
+            }
+            catch (Exception)
+            {
+                logger.LogError("Unable to request resource");
+            }
+
+            Console.ResetColor();
+        }
+
+        private static void LoadConfig()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            _configuration = builder.Build();
+        }
+
+        private static async Task ListStudents()
+        {
+            var students = await _apiClient.GetStudents();
+            Console.WriteLine($"Student Count: {students.Count()}");
+        }
+
+        private static async Task ListCourses()
+        {
+            var courses = await _apiClient.GetCourses();
+            Console.WriteLine($"Courses Count: {courses.Count()}");
+        }
+    }
+}
